6b3fd1999d7f9c93b131c666e59b7cb090bcc375
Guillaume Demay <guillaumedemay@hotmail.fr>
2024-09-07 14:00:23 +0200

    Initial Commit

---
+++ HabitFlow/HabitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitFlowApp.swift
+//  HabitFlow
+//
+//  Created by Guillaume Demay on 07.09.24.
+//
+
+import SwiftUI
+
+@main
+struct HabitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitFlowTests/HabitFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitFlowTests.swift
+//  HabitFlowTests
+//
+//  Created by Guillaume Demay on 07.09.24.
+//
+
+import Testing
+@testable import HabitFlow
+
+struct HabitFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
